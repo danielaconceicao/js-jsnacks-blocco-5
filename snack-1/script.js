c51f4e59011ea0d5599427be9407f58614290228
danielaconceicao <daniela.3597@gmail.com>
2024-10-16 15:18:10 +0200

    snack 1 finished

---
+++ snack-1/script.js
@@ -0,0 +1,7 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+ names.forEach(element => {
+    console.log(element) ;
+});
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
