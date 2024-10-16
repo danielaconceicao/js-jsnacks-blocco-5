f6b716c77439b31c5fc0f7d634b068d3287937f7
danielaconceicao <daniela.3597@gmail.com>
2024-10-16 15:35:13 +0200

    snack 3 finished

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,11 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+const newNumber = numbers.map(number => number + 1);
 // Risultato: [3, 9, 5, 8, 3, 88]
+console.log(newNumber);
+
+for (let i = 0; i < numbers.length; i++) {
+    const result = numbers[i] + 1;
+    console.log(result);
+}
