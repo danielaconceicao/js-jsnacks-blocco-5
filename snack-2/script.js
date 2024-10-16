01f7a643f1751940af797bdecb5a0e404ee99ffd
danielaconceicao <daniela.3597@gmail.com>
2024-10-16 15:27:35 +0200

    snack 2 finished

--- snack-2/script.js
+++ snack-2/script.js
@@ -5,4 +5,11 @@ const people = [
 ];
 
 // Stampa in console tutti i nomi
+const newPeople = people.map(person => person.name);
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+console.log(newPeople);
+
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+  console.log(person.name);
+}
